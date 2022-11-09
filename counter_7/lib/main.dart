f3b1632ce347ce70682e87a5a691b579ead4041a
morenoraw <moreno.rassya@ui.ac.id>
2022-11-10 01:22:30 +0700

    Revert "Telah menyelesaikan Lab 7."
    This reverts commit f78be84e13f2047fbc4b5112aa6f8226d00bea84.
    
    Revisi, salah push :(

--- counter_7/lib/main.dart
+++ counter_7/lib/main.dart
@@ -1,32 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:counter_7/form.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+  const MyApp({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(),
-        );
-    }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Program Counter',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Program Counter'),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
-    const MyHomePage({super.key});
+  const MyHomePage({super.key, required this.title});
 
-    final String title = 'Flutter Demo Home Page';
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
 
-    @override
-    State<MyHomePage> createState() => _MyHomePageState();
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -130,36 +148,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: const Icon(Icons.add),
               ),
           ],
-        ),
       ),
-      // Menambahkan drawer menu
-      drawer: Drawer(
-      child: Column(
-        children: [
-          // Menambahkan clickable menu
-          ListTile(
-            title: const Text('Counter'),
-            onTap: () {
-              // Route menu ke halaman utama
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const MyHomePage()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('Form'),
-            onTap: () {
-              // Route menu ke halaman form
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const MyFormPage()),
-              );
-            },
-          ),
-        ],
-      ),
-    ),
+      )
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _decrementCounter,
+      //   tooltip: 'Decrement',
+      //   child: const Icon(Icons.remove),
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
